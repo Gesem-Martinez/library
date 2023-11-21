464e0093fc95036cbc92d1e5e9d0d57751eb0edd
Gesem Martinez <ghmartinezmontes@gmail.com>
2023-11-20 19:37:33 -0600

    Add function to create book entries

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function Book(title, author, pages, hasBeenRead){
     this.hasBeenRead = hasBeenRead;
 
     this.info = function(){
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.hasBeenRead == false ? "not read yet" : "already read");
+        return `${this.title} by ${this.author}, ${this.pages} pages.`
+        //return this.title + " by " + this.author + ", " + this.pages + " pages.";
     }
 }
 
@@ -17,8 +18,48 @@ function addBookToLibrary(newBook){
     }
 }
 
+function createBookEntry(book){
+    let bookEntry = document.createElement("div");
+    bookEntry.classList.add("book-entry");
+    bookEntry.classList.add("black-border");
+
+    let bookTitle = document.createElement("h2");
+    let lineBreak = document.createElement("br");
+    let bookReadState = document.createElement("span");
+
+    let entryButtonsDiv = document.createElement("div");
+    let removeEntryButton = document.createElement("button");
+    let changeReadStateButton = document.createElement("button");
+    removeEntryButton.classList.add("remove-entry");
+    removeEntryButton.classList.add("black-border");
+    changeReadStateButton.classList.add("change-read-state");
+    changeReadStateButton.classList.add("black-border");
+
+    bookTitle.textContent = book.info();
+    if(book.hasBeenRead){
+        bookReadState.textContent = "Already Read";
+        bookReadState.classList.add("already-read");
+    } else {
+        bookReadState.textContent = "Not read yet";
+        bookReadState.classList.add("not-read");
+    }
+    bookTitle.appendChild(lineBreak);
+    bookTitle.appendChild(bookReadState);
+    
+    entryButtonsDiv.appendChild(removeEntryButton);
+    entryButtonsDiv.appendChild(changeReadStateButton);
+
+    bookEntry.appendChild(bookTitle);
+    bookEntry.appendChild(entryButtonsDiv);
+
+    return bookEntry;
+}
+
 function displayBooks(){
+    if(libraryArray.length == 0){
+        return ;
+    }
     for(let book in libraryArray){
-        
+
     }
 }
